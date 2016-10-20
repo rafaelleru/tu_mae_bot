39320add0646620fb69acd89c71ababe459aaebd
koji3 <jose.linares.1993@gmail.com>
2016-10-20 21:32:18 +0200

    Creo que ya si

--- tu_mae_bot.py
+++ tu_mae_bot.py
@@ -43,17 +43,27 @@ def recibe(messages):
                         bot.send_message(m.chat.id, '/spam')
 
             ran = random.randint(1,200)
-            if(ran==2):
+            if ran==2 :
                 import urllib
+                import random
                 ran1 = random.randint(100, 500)
                 ran2 = random.randint(1, 3)
                 f = open('1.jpg', 'wb')
-                if(ran2==1):
-                    f.write(urllib.urlopen('https://www.placecage.com/'+ran1+'/'+ran1).read())
-                if(ran2==2):
-                    f.write(urllib.urlopen('https://www.placecage.com/g/'+ran1+'/'+ran1).read())
-                if(ran2==3):
-                    f.write(urllib.urlopen('https://www.placecage.com/c/'+ran1+'/'+ran1).read())
+                if ran2==1 :
+                    string='https://www.placecage.com/'
+
+                if ran2==2 :
+                    string='https://www.placecage.com/g/'
+
+                if ran2==3 :
+                    string='https://www.placecage.com/c/'
+        
+
+                string+=str(ran1)
+                string+='/'
+                string+=str(ran1)
+                f.write(urllib.urlopen(string).read())
+
                 f.close()
 
                 photo = open('1.jpg', 'rb')
